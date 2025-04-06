47fd897aa20498130f2089d9b58be37ea57e1289
haero77 <shineecard5@gmail.com>
2025-04-07 00:59:24 +0900

    initial commit

---
+++ backend/goforawalk/src/test/kotlin/devclub/goforawalk/GoforawalkApplicationTests.kt
@@ -0,0 +1,12 @@
+package devclub.goforawalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoforawalkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
