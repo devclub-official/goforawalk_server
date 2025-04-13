dfa829d0f52b7190c667c3f6a7245086be3687a5
haero77 <shineecard5@gmail.com>
2025-04-13 14:00:24 +0900

    app-server initial commit

---
+++ app-server/src/test/kotlin/side/flab/goforawalk/GoforawalkApplicationTests.kt
@@ -0,0 +1,12 @@
+package side.flab.goforawalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoforawalkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
