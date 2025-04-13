027483bce32785c77da5df4c4c95ed5f3b9e43e0
haero77 <shineecard5@gmail.com>
2025-04-13 16:15:37 +0900

    feat: add sample GET, POST API

---
+++ backend/app-server/src/main/kotlin/side/flab/goforawalk/sample/SampleApi.kt
@@ -0,0 +1,29 @@
+package side.flab.goforawalk.sample
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/sample")
+class SampleApi {
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello World!"
+    }
+
+    @PostMapping("/hello")
+    fun helloWorld(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hello ${request.name}!")
+    }
+
+    data class HelloRequest(
+        val name: String
+    )
+
+    data class HelloResponse(
+        val message: String
+    )
+}
