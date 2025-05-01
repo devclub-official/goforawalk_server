35450cf1aefa2ccfdc09d5990fa447e1ad7fd4fd
haero77 <shineecard5@gmail.com>
2025-05-01 19:49:42 +0900

    [GW-59] test: add a rest-assured test component

--- backend/app-server/src/test/kotlin/side/flab/goforawalk/support/BaseIntegrationTest.kt
+++ backend/app-server/src/test/kotlin/side/flab/goforawalk/support/BaseIntegrationTest.kt
@@ -1,7 +1,8 @@
 package side.flab.goforawalk.support
 
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Profile
 
+@Profile("test")
 @SpringBootTest
-abstract class BaseIntegrationTest {
-}
+abstract class BaseIntegrationTest
