ceb1e43fcd4129a9a4c7777de568f989bd0e72f2
haero77 <shineecard5@gmail.com>
2025-05-05 22:24:17 +0900

    [GW-59] test: wiremock 포트 설정을 테스트 서포트 추상 클래스에서 설정하도록 변경

--- backend/app-server/src/test/kotlin/side/flab/goforawalk/app/support/BaseRestAssuredTest.kt
+++ backend/app-server/src/test/kotlin/side/flab/goforawalk/app/support/BaseRestAssuredTest.kt
@@ -4,11 +4,13 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.test.web.servlet.MockMvc
 import kotlin.test.BeforeTest
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@AutoConfigureWireMock(port = 8089)
 abstract class BaseRestAssuredTest : BaseIntegrationTest() {
     @Autowired
     lateinit var mockMvc: MockMvc
